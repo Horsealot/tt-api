1eaca658bcd52b6a3c473498d29a8679e3396aec
Thomas <desgrippes.thomas@gmail.com>
2019-10-30 10:05:12 +0100

    TRIKTRAK-50 Add SessionResponse tests

--- api/models/responses/session.response.js
+++ api/models/responses/session.response.js
@@ -2,7 +2,7 @@ class SessionResponse {
     constructor(session, previousSelectionCompleted) {
         this.start_at = session.start_at;
         this.end_at = session.end_at;
-        this.inProgress = session.start_at <= new Date();
+        this.in_progress = session.start_at <= new Date() && session.end_at >= new Date();
         this.previous_selection_completed = previousSelectionCompleted;
     }
 }
---
+++ test/api/models/responses/session.response.test.js
@@ -0,0 +1,48 @@
+process.env.NODE_ENV = 'test';
+
+require('module-alias/register');
+require('dotenv').config({path: '.env.test'});
+
+//Require the dev-dependencies
+const chai = require('chai');
+const expect = chai.expect;
+const moment = require('moment');
+
+const SessionResponse = require('@models/responses/session.response');
+
+describe('Session Response', () => {
+    it('should work for a session in progress', (done) => {
+        const startDate = moment().subtract(1, 'hours');
+        const endDate = moment().add(1, 'hours');
+        const sessionResponse = new SessionResponse({
+            start_at: startDate,
+            end_at: endDate,
+        }, true);
+        expect(sessionResponse.start_at).to.be.eq(startDate);
+        expect(sessionResponse.end_at).to.be.eq(endDate);
+        expect(sessionResponse.in_progress).to.be.true;
+        expect(sessionResponse.previous_selection_completed).to.be.true;
+        expect((new SessionResponse({
+            start_at: startDate,
+            end_at: endDate,
+        }, false)).previous_selection_completed).to.be.false;
+        done();
+    });
+    it('should work for a session in progress', (done) => {
+        const startDate = moment().subtract(2, 'days');
+        const endDate = moment().subtract(1, 'days');
+        const sessionResponse = new SessionResponse({
+            start_at: startDate,
+            end_at: endDate,
+        }, true);
+        expect(sessionResponse.start_at).to.be.eq(startDate);
+        expect(sessionResponse.end_at).to.be.eq(endDate);
+        expect(sessionResponse.in_progress).to.be.false;
+        expect(sessionResponse.previous_selection_completed).to.be.true;
+        expect((new SessionResponse({
+            start_at: startDate,
+            end_at: endDate,
+        }, false)).previous_selection_completed).to.be.false;
+        done();
+    });
+});
